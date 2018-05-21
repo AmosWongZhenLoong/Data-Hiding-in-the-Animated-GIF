bb32419bfdd3bcf40ad84090390e9a0c6541d8d0
AmosWongZhenLoong <azwon1@student.monash.edu>
2018-05-21 12:38:29 +0800

    core functionality complete
    need to to cleanup

--- FuncLib.py
+++ FuncLib.py
@@ -22,6 +22,7 @@ def parse(filename):
     delayLocations = []
     delayTimeCart = []
     GCBCart = []
+    frameTypeCart = []
 
     binary_file = open(filename, 'rb')
     binary_file.seek(0)
@@ -105,16 +106,16 @@ def parse(filename):
             print(LZW[0])
             index += 1
             blocksize = binary_file.read(1)
+
+            # takes note of the transparent 1x1 pixel frames (for decoding purposes)
+            if blocksize[0] == 4:
+                frameTypeCart.append(1)
+            else:
+                frameTypeCart.append(0)
+
             index += 1
             while blocksize[0] != 0:
                 imgData = binary_file.read(blocksize[0])
-                #textfile = open('imgdataEARTHdebug.txt','a+')
-                #or byte in range(len(imgData)):
-                #    textfile.write(str(imgData[byte])+" ")
-                #    if byte%40 == 0:
-                #        textfile.write('\n')
-                #textfile.write('\n')
-                #textfile.write('\n')
                 index += blocksize[0]
                 blocksize = binary_file.read(1)
                 index += 1
@@ -136,7 +137,7 @@ def parse(filename):
 
     binary_file.close()
 
-    return delayLocations, delayTimeCart, endIndex, GCBCart, CTindex, globalColorTableSize, globalColorTable
+    return delayLocations, delayTimeCart, endIndex, GCBCart, CTindex, globalColorTableSize, globalColorTable, frameTypeCart
 
 
 def getCharCapacity(delayTimeCart):
@@ -160,6 +161,18 @@ def msg2bits(msg):
     return bitstream
 
 
+def msgfrombits(bits):
+    # convert bitstream to text
+    # output: text
+    msg = ""
+    for i in range(len(bits)):
+        if i % 8 == 0:
+            byte = bits[i:i+8]
+            char = chr(int(byte,2))
+            msg = msg + char
+    return msg
+
+
 def modifyDelayTimes(bitstream, delayTimeCart):
     # modify the delayTimeCart to store the message
     # message representation:
@@ -181,3 +194,206 @@ def writeToFile(filename, delayTimeCart, delayLocations):
             binary_file.seek(delayLocations[i])
             binary_file.write(delayTimeCart[i])
         binary_file.close()
+
+
+def encode(delayTimeCart,msg,minDelay):
+    # encodes the structure of the new frames in the form of delay values
+    # minimum frame delay for chrome, firefox, and opera: 0.02s
+    # minimum frame delay safari and IE: 0.06s
+    # info as of: http://nullsleep.tumblr.com/post/16524517190/animated-gif-minimum-frame-delay-browser
+    # error handle -1: insufficient capacity to store message
+    newDelayTimeCart = []
+    group = []
+
+    delayTimeCartIndex = 0
+    i = 0
+
+    delay = delayTimeCart[delayTimeCartIndex]
+    group.append(minDelay)
+
+    while i < len(msg):
+        # original frame still has capacity
+        if sum(group) < delay:
+            # next appending frame exceeds capacity
+            if int(msg[i])+(minDelay) + sum(group) > delay:
+                pad_amount = delay - sum(group)
+                group[0] = group[0] + pad_amount
+                i -= 1
+            else:
+                group.append(int(msg[i])+minDelay)
+        # last appended frame fits capacity perfectly
+        # reset stuff
+        if sum(group) == delay:
+            newDelayTimeCart.append(group)
+            group = []
+            group.append(minDelay)
+            delayTimeCartIndex += 1
+            # out of original frames for storage
+            if delayTimeCartIndex >= len(delayTimeCart):
+                return -1
+            delay = delayTimeCart[delayTimeCartIndex]
+        i += 1
+
+    # half an original frame filled
+    if len(group) > 1:
+        newDelayTimeCart.append(group)
+
+    # pad the last frame so that total delay is correct
+    pad_amount = delay - sum(newDelayTimeCart[-1])
+    if pad_amount != 0:
+        newDelayTimeCart[-1][0] = newDelayTimeCart[-1][0] + pad_amount
+
+
+    return newDelayTimeCart
+
+
+def writeOriginal(newFile, binary_file, GCBCart, i, byteOrder, delay):
+    binary_file.seek(GCBCart[i-1])
+    uChunk = binary_file.read(3)  # gcb identifier + blocksize
+    packedfields = binary_file.read(1)  # packed fields
+    dtChunk = binary_file.read(2)  # delay time
+    tciChunk = binary_file.read(1)  # transparent color index
+    nChunk = binary_file.read(6)  # terminator + identifier + img positions
+    u21Chunk = binary_file.read(2)  # img width
+    u22Chunk = binary_file.read(2)  # img height
+    packedfields2 = binary_file.read(1)  # packed fields
+    lChunk = binary_file.read(GCBCart[i] - GCBCart[i - 1] - 18)  # image data
+
+    # changing disposal method so that each frame remains on screen
+    packedfields = intToBytes(5, byteOrder, 1)
+
+    # change delay
+    if delay != -1:
+        dtChunk = intToBytes(delay,byteOrder,2)  # delay time
+
+    # test change transparent color flag
+    #tciChunk = intToBytes(0,byteOrder,1)
+
+    # append to file
+    newFile.write(uChunk)
+    newFile.write(packedfields)
+    newFile.write(dtChunk)
+    newFile.write(tciChunk)
+    newFile.write(nChunk)
+    newFile.write(u21Chunk)
+    newFile.write(u22Chunk)
+    newFile.write(packedfields2)
+    newFile.write(lChunk)
+
+
+def writeTransparent(newFile, binary_file, GCBCart, i, byteOrder, delay):
+    binary_file.seek(GCBCart[i-1])
+    uChunk = binary_file.read(3)  # gcb identifier + blocksize
+    packedfields = binary_file.read(1)  # packed fields
+    dtChunk = binary_file.read(2)  # delay time
+    tciChunk = binary_file.read(1)  # transparent color index
+    nChunk = binary_file.read(6)  # terminator + identifier + img positions
+    u21Chunk = binary_file.read(2)  # img width
+    u22Chunk = binary_file.read(2)  # img height
+    packedfields2 = binary_file.read(1)  # packed fields
+
+    # changing disposal method so that each frame remains on screen
+    packedfields = intToBytes(5, byteOrder, 1)
+
+    # change delay
+    dtChunk = intToBytes(delay, byteOrder, 2)  # delay time
+
+    # test change transparent color flag
+    #tciChunk = intToBytes(0,byteOrder,1)
+
+    # changing width and height of transparent frame to 1px
+    u21Chunk = intToBytes(1, byteOrder, 2)
+    u22Chunk = intToBytes(1, byteOrder, 2)
+
+    # append to file
+    newFile.write(uChunk)
+    newFile.write(packedfields)
+    newFile.write(dtChunk)
+    newFile.write(tciChunk)
+    newFile.write(nChunk)
+    newFile.write(u21Chunk)
+    newFile.write(u22Chunk)
+    newFile.write(packedfields2)
+
+    # writing image data of a transparent frame
+    lChunk = intToBytes(8, byteOrder, 1)    #LZW
+    newFile.write(lChunk)
+    lChunk = intToBytes(4, byteOrder, 1)    #blocksize
+    newFile.write(lChunk)
+    lChunk = intToBytes(0, byteOrder, 1)    #byte1
+    newFile.write(lChunk)
+    lChunk = intToBytes(255, byteOrder, 1)  #byte2
+    newFile.write(lChunk)
+    lChunk = intToBytes(5, byteOrder, 1)    #byte3
+    newFile.write(lChunk)
+    lChunk = intToBytes(4, byteOrder, 1)    #byte4
+    newFile.write(lChunk)
+    lChunk = intToBytes(0, byteOrder, 1)    #end of LZW
+    newFile.write(lChunk)
+
+
+def getHidingCapacity(delayTimeCart,minDelay):
+    # calculates average hiding capacity based on:
+    # total number of frames, average additional frames per frame, minimum delay specified by user
+    # returns average characters that can be hidden
+    totalAvgPerFrame = 0
+    for i in range(len(delayTimeCart)):
+        frameCap = delayTimeCart[i]
+        minBitsPerFrame = math.floor((frameCap - minDelay) / minDelay)
+        maxBitsPerFrame = math.floor((frameCap - minDelay) / (minDelay + 1))
+        avgBitsPerFrame = ((minBitsPerFrame + maxBitsPerFrame) / 2)
+        totalAvgPerFrame += avgBitsPerFrame
+    avgBitsPerFrame = (totalAvgPerFrame / len(delayTimeCart))
+    estChars = math.floor((len(delayTimeCart) * avgBitsPerFrame) / 8)
+    return estChars
+
+
+def decodePrep(delayTimeCart,frameTypeCart):
+    bitValuesUsed = []
+    oriDelayTimeCart = []
+    tempDelay = []
+
+    for i in range(len(delayTimeCart)):
+        if frameTypeCart[i] == 0 and len(tempDelay) == 0:
+            tempDelay.append(delayTimeCart[i])
+        elif frameTypeCart[i] == 1 and len(tempDelay) != 0:
+            tempDelay.append(delayTimeCart[i])
+            if delayTimeCart[i] not in bitValuesUsed:
+                bitValuesUsed.append(delayTimeCart[i])
+        elif frameTypeCart[i] == 0 and len(tempDelay) != 0:
+            oriDelayTimeCart.append(tempDelay)
+            tempDelay = []
+            tempDelay.append(delayTimeCart[i])
+        elif i == len(delayTimeCart) - 1:
+            tempDelay.append(delayTimeCart[i])
+            oriDelayTimeCart.append(tempDelay)
+            delay = []
+            if delayTimeCart[i] not in bitValuesUsed:
+                bitValuesUsed.append(delayTimeCart[i])
+
+    if len(tempDelay) > 0:
+        oriDelayTimeCart.append(tempDelay)
+
+    return bitValuesUsed,oriDelayTimeCart
+
+
+def decode(bitValuesUsed,frameTypeCart,delayTimeCart):
+    bits = ""
+    msg = ""
+
+    for i in range(len(frameTypeCart)):
+        if frameTypeCart[i] == 1:
+            if delayTimeCart[i] == bitValuesUsed[0]:
+                bits += "0"
+            elif delayTimeCart[i] == bitValuesUsed[1]:
+                bits += "1"
+            if len(bits) == 8:
+                d = int(bits,2)
+                char = chr(d)
+                msg += char
+                bits = ""
+
+    return msg
+
+
+
--- parsegifs - dupeFrames.py
+++ parsegifs - dupeFrames.py
@@ -1,154 +1,181 @@
 from pprint import pprint
 import struct
 import codecs
+import time
 
 import FuncLib as FuncLib
 
 
 
-filename = "rotatingearthCopy.gif"
+filename = "oilpump.gif"
 outputfilename = 'modifiedgif.gif'
+reconstructedfilename = 'reconstructedgif.gif'
 byteOrder = 'little'
 
-
-delayLocations,delayTimeCart,endIndex,GCBCart,CTindex,globalColorTableSize,globalColorTable = FuncLib.parse(filename)
+# get essentials
+delayLocations,delayTimeCart,endIndex,GCBCart,CTindex,globalColorTableSize,globalColorTable,frameTypeCart = FuncLib.parse(filename)
 delayTimeCart = FuncLib.intFromBytes(delayTimeCart,byteOrder)
 
 print(delayLocations)
 print(delayTimeCart)
 print(GCBCart)
+print(frameTypeCart)
 
-newFile = open(outputfilename,'a+b')
-binary_file = open(filename,'r+b')
+# choose encode or decode
+print("do you want to do encoding or decoding?")
+action = str(input("type e for encoding, type d for decoding: "))
 
-# write the first part of the file up till the first graphics control block
-binary_file.seek(0)
-initialChunk = binary_file.read(GCBCart[0])
-newFile.write(initialChunk)
-i = 1
-while i < len(GCBCart):
-    uChunk = binary_file.read(3)        # gcb part 1
-    pfields = binary_file.read(1)       # packed fields
-    uuChunk = binary_file.read(5)       # gcb part 2 + img descriptor identifier
-    u11Chunk = binary_file.read(2)      # image left position
-    u12Chunk = binary_file.read(2)      # image top position
-    u21Chunk = binary_file.read(2)      # image width
-    u22Chunk = binary_file.read(2)      # image height
-    u3Chunk = binary_file.read(1)       # packed fields
-    lChunk = binary_file.read(GCBCart[i]-GCBCart[i-1]-18)   # image data
-    newFile.write(uChunk)
-    pfields = FuncLib.intToBytes(5,byteOrder,1)
-    newFile.write(pfields)
-    newFile.write(uuChunk)
-    newFile.write(u11Chunk)
-    newFile.write(u12Chunk)
-    newFile.write(u21Chunk)
-    newFile.write(u22Chunk)
-    newFile.write(u3Chunk)
-    newFile.write(lChunk)
-    '''print(uChunk)
-    print(pfields)
-    print(uuChunk)
-    print(u11Chunk)
-    print(u12Chunk)
-    print(u21Chunk)
-    print(u22Chunk)
-    print(u3Chunk)'''
+if action == 'e':       # do encoding
 
-    #u11Chunk = FuncLib.intToBytes(95, byteOrder, 2)
-    #u12Chunk = FuncLib.intToBytes(95, byteOrder, 2)
-    u21Chunk = FuncLib.intToBytes(1, byteOrder, 2)
-    u22Chunk = FuncLib.intToBytes(1, byteOrder, 2)
+    # web browser compatibility selection
+    print("Different browsers support different minimum delay values")
+    print("Lower values increase hiding capacity, but decrease playback accuracy with some browsers")
+    print("Reference guide for the 5 most popular browsers (values are in 1/100th of a second): ")
+    print("Chrome/Firefox/Opera: 2 | Safari,IE: 6")
+    minDelay = int(input("Enter desired minimum delay: "))
 
-    newFile.write(uChunk)
-    newFile.write(pfields)
-    newFile.write(uuChunk)
-    newFile.write(u11Chunk)
-    newFile.write(u12Chunk)
-    newFile.write(u21Chunk)
-    newFile.write(u22Chunk)
-    newFile.write(u3Chunk)
+    # hiding capacity
+    charCap = FuncLib.getHidingCapacity(delayTimeCart,minDelay)
+    if charCap < 1:
+        print("unable to hide data, as file contains delays that are too small")
+        time.sleep(3)
+        quit()
+    else:
+        print("the estimated text capacity is %d characters" % charCap)
 
-    lChunk = FuncLib.intToBytes(8, byteOrder, 1)    #LZW
-    newFile.write(lChunk)
-    lChunk = FuncLib.intToBytes(4, byteOrder, 1)    #blocksize
-    newFile.write(lChunk)
-    lChunk = FuncLib.intToBytes(0, byteOrder, 1)    #byte1
-    newFile.write(lChunk)
-    lChunk = FuncLib.intToBytes(255, byteOrder, 1)  #byte2
-    newFile.write(lChunk)
-    lChunk = FuncLib.intToBytes(5, byteOrder, 1)    #byte3
-    newFile.write(lChunk)
-    lChunk = FuncLib.intToBytes(4, byteOrder, 1)    #byte4
-    newFile.write(lChunk)
+    # get message from user
+    userinput = str(input("Enter message here: "))
 
-    lChunk = FuncLib.intToBytes(0, byteOrder, 1)    #end of LZW
-    newFile.write(lChunk)
+    # convert message to bits
+    userinputbits = FuncLib.msg2bits(userinput)
 
-    '''print(uChunk)
-    print(u11Chunk)
-    print(u12Chunk)
-    print(u21Chunk)
-    print(u22Chunk)
-    print(u3Chunk)'''
+    print(userinput)
+    print(userinputbits)
 
-    i = i + 1
+    # encode the message
+    print("encoding the message")
+    newDelayTimeCart = FuncLib.encode(delayTimeCart,userinputbits,minDelay)
+    if newDelayTimeCart == -1:
+        print("insufficient capacity to store message")
+        time.sleep(3)
+        quit()
+    print(newDelayTimeCart)
 
-while True:
-    fileChunk = binary_file.read(1)
-    if not fileChunk:
-        break
-    newFile.write(fileChunk)
+    # create a new file for writing
+    newFile = open(outputfilename,'a+b')
+    binary_file = open(filename,'r+b')
 
-newFile.close()
-binary_file.close()
-
-
-#todo: put one pixel of the transparent color in between every frame
-#todo: set original frame to delay = 1
-#todo: stack as many transparent frames on top
-
-'''
-# duplicate every frame
-newFile = open(outfilename,'a+b')
-with open(filename,'r+b') as binary_file:
-    # write the beginning portion of the file up till the first graphic control block
+    # write the first part of the file up till the first graphics control block
+    print("writing first part of file")
     binary_file.seek(0)
-    fileChunk = binary_file.read(GCBCart[0]-1)
-    newFile.write(fileChunk)
-    # duplicate each frame up till the second last frame
-    for i in range(len(GCBCart)-1):
-        fileChunk = binary_file.read(GCBCart[i+1]-GCBCart[i])
-        newFile.write(fileChunk)
-        newFile.write(fileChunk)
-    # duplicate last frame
-    fileChunk = binary_file.read(end-GCBCart[-1])
-    newFile.write(fileChunk)
-    newFile.write(fileChunk)
-    # write the rest of the file
+    initialChunk = binary_file.read(GCBCart[0])
+    newFile.write(initialChunk)
+
+    print("writing middle portion of file")
+    i = 1
+    j = 0
+
+    while i < len(GCBCart):
+        if j < len(newDelayTimeCart):
+            sequence = newDelayTimeCart[j]
+            for index in range(len(sequence)):
+                delay = sequence[index]
+                if index == 0:
+                    FuncLib.writeOriginal(newFile,binary_file,GCBCart,i,byteOrder,delay)
+                    print('wrote original"')
+                else:
+                    FuncLib.writeTransparent(newFile,binary_file,GCBCart,i,byteOrder,delay)
+                    print('wrote transparent')
+        else:
+            FuncLib.writeOriginal(newFile,binary_file,GCBCart,i,byteOrder,-1)
+            print('wrote unchanged')
+
+        j += 1
+        i += 1
+
+    # write everything else after the last image data
+    # up till the end of file
+    print("writing last part of file")
     while True:
         fileChunk = binary_file.read(1)
         if not fileChunk:
             break
         newFile.write(fileChunk)
-newFile.close()
-binary_file.close()
 
-graphicControl1 = filescan(outfilename)
-GCBCart, delayTimeCart, end = frameScan(outfilename,graphicControl1)
-
-# half the duration for each frame
-for i in range(len(delayTimeCart)):
-    item = delayTimeCart[i]//2
-    newitem = item.to_bytes(2,'little',signed=False)
-    delayTimeCart[i] = newitem
-print(delayTimeCart)
-
-# writing new delay times
-with open(outfilename,"r+b") as binary_file:
-    for i in range(len(delayTimeCart)):
-        binary_file.seek(GCBCart[i]+4)
-        binary_file.write(delayTimeCart[i])
+    newFile.close()
     binary_file.close()
+    print("process complete")
 
-'''
+elif action == 'd':     # do decoding
+    # get some information necessary for msg decoding and original GIF reconstruction
+    bitValuesUsed,oriDelayTimeCart = FuncLib.decodePrep(delayTimeCart,frameTypeCart)
+    bitValuesUsed.sort()
+    print(bitValuesUsed)
+    print(len(oriDelayTimeCart))
+
+    # recover proper delay values
+    for i in range(len(oriDelayTimeCart)):
+        oriDelayTimeCart[i] = sum(oriDelayTimeCart[i])
+    print(len(oriDelayTimeCart))
+
+    msg = FuncLib.decode(bitValuesUsed,frameTypeCart,delayTimeCart)
+    print(msg)
+
+    # reconstruct original GIF image
+    file = open(reconstructedfilename,'a+b')
+    file2 = open('modifiedgif.gif','r+b')
+
+    # write everything up till the first graphics control block
+    file2.seek(0)
+    file_beginning = file2.read(GCBCart[0])
+    file.write(file_beginning)
+
+    # write all the original frames only
+    j = 0
+    for i in range(len(GCBCart)):
+        if frameTypeCart[i] == 0 and i == len(GCBCart) - 1:
+            file2.seek(GCBCart[i])
+            fchunk = file2.read(4)
+            dchunk = file2.read(2)
+            lchunk = file2.read(endIndex - GCBCart[i] - 6)
+
+            dchunk = FuncLib.intToBytes(oriDelayTimeCart[j],'little',2)
+            j += 1
+
+            file.write(fchunk)
+            file.write(dchunk)
+            file.write(lchunk)
+        elif frameTypeCart[i] == 0:
+            file2.seek(GCBCart[i])
+            fchunk = file2.read(4)
+            dchunk = file2.read(2)
+            lchunk = file2.read(GCBCart[i + 1] - GCBCart[i] - 6)
+
+            dchunk = FuncLib.intToBytes(oriDelayTimeCart[j], 'little', 2)
+            j += 1
+
+            file.write(fchunk)
+            file.write(dchunk)
+            file.write(lchunk)
+
+    # write remainder of file
+    if file2.tell() < endIndex - 1:
+        file2.seek(endIndex - 1)
+        chunk = file2.read(1)
+        while chunk:
+            file.write(chunk)
+            chunk = file2.read(1)
+
+    file.close()
+    file2.close()
+
+    print('decode complete')
+
+
+
+else:                   # quit?
+    print("invalid command")
+    time.sleep(3)
+    quit()
+
+
